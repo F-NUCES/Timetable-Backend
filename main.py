a6a6683f77d91cad1309c126ebb2180360824bb6
Rafay Ghafoor <rafayghafoor@pm.me>
2020-07-19 10:49:58 +0500

    feat(root): updated root endpoint to show docs

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import database as db
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
-
+    return RedirectResponse("/docs")
+    
 @app.get("/courses")
 async def courses():
     return JSONResponse(db.list_courses())
