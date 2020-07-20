5a5a74edb08a90ac8cf669b2794aa8b0d68f3dba
Rafay Ghafoor <rafayghafoor@pm.me>
2020-07-20 12:42:15 +0500

    feat(deployment): update dockerfile

--- src/main.py
+++ src/main.py
@@ -2,8 +2,24 @@ import database as db
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
